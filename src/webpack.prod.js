7bf003bb512b6d30d46c5cc9097b889faad503fd
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-05-04 01:30:26 +0700

    clean repo

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,5 +8,4 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].bundle.js',
   },
-  plugins: [new CleanWebpackPlugin],
 });
