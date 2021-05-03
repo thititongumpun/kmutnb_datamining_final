b846db0f969d40dccc407295538772c1bcb81bbe
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-05-03 20:54:05 +0700

    asd

--- src/js/index.js
+++ src/js/index.js
@@ -119,7 +119,7 @@ class Model {
 
 	loadModel() {
     console.time("Load model");
-    return tf.loadLayersModel("../dist/model/model.json").then(model => {
+    return tf.loadLayersModel("../model/model.json").then(model => {
       this._model = model;
       console.timeEnd("Load model")
     });
