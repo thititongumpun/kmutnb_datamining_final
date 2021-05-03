f5a2f34761c12c69daabe5b40896b2b747ca783e
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-05-04 01:36:54 +0700

    url imgs

--- src/webpack.common.js
+++ src/webpack.common.js
@@ -25,7 +25,6 @@ module.exports = {
           loader: "file-loader",
           options: {
             name: "[name].[hash].[ext]",
-            outputPath: 'images',
           }
         }
       },
